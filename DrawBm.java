5805851bc356766bb72fd4df4440963d38e47366
Sasaki Moe <sasakimoe@sasaki-no-MacBook-Air.local>
2017-11-29 14:15:13 +0900

    1129

--- DrawBm.java
+++ DrawBm.java
@@ -2,33 +2,44 @@ package in.andante.drawbm;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-import android.database.sqlite.SQLiteDatabase;//SQLite用
 
 public class DrawBm extends Activity {
 
 PenView penview;
-	
+//DBHelper dbhelper;
+DBHelper db0;
+public Activity _context;
+
 /*Called when the activity is first created.*/
 @Override
 public void onCreate(Bundle savedInstanceState) {
+	
 	super.onCreate(savedInstanceState);
-	penview= new PenView(this);
+	penview = new PenView(this);//PenViewクラスのインスタンス生成
 	setContentView(penview);
-	//setContentView(R.layout.activity_sub);
+    //Exchange exchange = (Exchange) this.getApplication();
+    //String str = exchange.getTestString();
+	//dbhelper = new DBHelper(this);
+	db0 = new DBHelper(this);  //DBHelperクラスのインスタンス生成
+	SQLiteDatabase db1 = db0.getWritableDatabase();//読み書き用
+	db1.close();
+
+
 }
 
 /*メニューの生成イベント*/
 @Override
 public boolean onCreateOptionsMenu(Menu menu) {//menu->draw_bm.xmlで記述
-
+	
 	super.onCreateOptionsMenu(menu);
 	getMenuInflater().inflate(R.menu.draw_bm,menu);  
 	return true;
-	
+
 }
 
 /*メニューがクリックされた時のイベント*/
@@ -40,8 +51,8 @@ public boolean onOptionsItemSelected(MenuItem item) {
 			penview.clearDrawList(); 
 			break;
 		case R.id.item2:
-			penview.insert();
 			penview.saveToFile();
+			db0.insert();
 			penview.clearDrawList(); 
 			break;
 		case R.id.item3:
@@ -52,4 +63,4 @@ public boolean onOptionsItemSelected(MenuItem item) {
 	return true;
 }
 
-}//クラスDrawBm終了
+}
